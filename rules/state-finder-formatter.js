ea15250091f63b30e365bdcd26619afda33bf023
mf93 <Matt.Feld@sas.com>
2017-09-07 17:27:28 -0400

    added template test file and a custom formatter (just super basic for now)

---
+++ rules/state-finder-formatter.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mafeld on 9/7/2017.
+ */
+//Believe it or not, this is a fully functioning formatter!!!
+module.exports = function (results) {
+    console.log(JSON.stringify(results, null, 2));
+}
